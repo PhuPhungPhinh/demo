44955e263eab9dc7a1933241044ef43de790e52f
PhuPhungPhinh <164875564+PhuPhungPhinh@users.noreply.github.com>
2025-02-27 15:42:57 +0700

    linhtinh

---
+++ src/test/java/org/example/Sledium/sledium.java
@@ -0,0 +1,42 @@
+package org.example.Sledium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class sledium {
+//    WebDriver driver;
+//
+//    @BeforeEach
+//    public void setup() {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//    }
+//
+//
+//    @Test
+//    void testWeb() throws InterruptedException {
+//        driver.get("https://github.com/");
+//
+//        driver.findElement(By.xpath("/html/body/div[1]/div[3]/header/div/div[2]/div/div/div/a")).click();
+//
+//        Thread.sleep(2000);
+//
+//        driver.findElement(By.xpath("//*[@id=\"login_field\"]")).sendKeys("PhuPhungPhinh");
+//        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("Phuhoang205@T");
+//        driver.findElement(By.xpath("//*[@id=\"login\"]/div[4]/form/div/input[13]")).click();
+//
+//        WebElement webElement = driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div/div[1]/div/div[2]/nav/ul/li/a/span"));//link cais muon test
+//
+//        String actualFirstCategory = webElement.getText();//lay gia trij text tren link chuyen vao
+//        String expectedFirstCategory = "Dashboard";
+//        Assertions.assertEquals(expectedFirstCategory, actualFirstCategory);
+//
+//    }
+}
